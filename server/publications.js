1e44960f35593f24493cd8ee33a5251d45af7929
oanylund <ole.a.nylund@gmail.com>
2016-02-19 14:27:56 +0100

    Added ingredient publications

--- server/publications.js
+++ server/publications.js
@@ -3,7 +3,9 @@
 Meteor.publish("uker", function () {
     return Uker.find({});
 });
-
 Meteor.publish("ingrediensKat", function () {
     return IngrediensKat.find({});
+});
+Meteor.publish("ingredienser", function () {
+    return Ingredienser.find({});
 });
