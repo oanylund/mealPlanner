3c0f9bfeb9efcd9d0e7ac3315a4be21a0105b692
oanylund <ole.a.nylund@gmail.com>
2016-02-19 13:23:58 +0100

    Removed semicolons

--- client/app/components/Ingredienser/Kategorier/IngrediensKategorier.jsx
+++ client/app/components/Ingredienser/Kategorier/IngrediensKategorier.jsx
@@ -2,16 +2,18 @@ import React, { PropTypes } from 'react'
 import {composeWithTracker} from 'react-komposer';
 
 function composer(props, onData) {
-  const handle = Meteor.subscribe('ingrediensKat');
+  const handle = Meteor.subscribe('ingrediensKat')
   if (handle.ready()) {
-    const categories = IngrediensKat.find({}, {sort: {_id: 1}}).fetch();
-    onData(null, {categories});
+    const categories = IngrediensKat.find({}, {sort: {_id: 1}}).fetch()
+    onData(null, {categories})
   };
 };
 
-const IngKatList = (props) => {
-  const categoryList = categories.map( (cat) => {
-    <p>{cat.name}</p>
+const IngKatList = ({categories}) => {
+  const categoryList = categories.map( (category) => {
+    return(
+      <p>{category.name}</p>
+    )
   })
 
   return (
