851f3e8a25196e98e330fe0770ff805b8bed6a2d
oanylund <ole.a.nylund@gmail.com>
2016-02-20 05:09:50 +0100

    Added sorting on composers. Categories and Ingredients are now in ascending order based on name

--- client/app/composers/IngredsInCat.js
+++ client/app/composers/IngredsInCat.js
@@ -1,7 +1,7 @@
 export default function composer(props, onData) {
   const handle = Meteor.subscribe('ingredienser')
   if (handle.ready()) {
-    const IngredsInCat = Ingredienser.find({ categoryId: props.category._id }).fetch()
+    const IngredsInCat = Ingredienser.find({ categoryId: props.category._id }, { sort: { 'name.singular': 1 } }).fetch()
     onData(null, {IngredsInCat})
   }
 }
