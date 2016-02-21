cd51e0c2640c0db0d0f374890f96197be195a53a
oanylund <ole.a.nylund@gmail.com>
2016-02-21 02:26:31 +0100

    Created component for editing ingredient category inline in header

---
+++ client/app/components/Ingredienser/IngrediensHeaderEdit.jsx
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react'
+
+class IngrediensHeaderEdit extends React.Component {
+  constructor(props) {
+    super(props)
+    this.onEditSubmit = this.onEditSubmit.bind(this)
+  }
+  componentDidMount() {
+    this.refs[this.props.name].focus()
+  }
+  onEditSubmit(e) {
+    e.preventDefault()
+
+    let newName = this.refs[this.props.name].value
+    Meteor.call('updateIngredientCategory', this.props.id, newName, (err,res) => {
+      this.props.close()
+    })
+  }
+  render () {
+    let { name, close } = this.props
+    return (
+      <form onSubmit={this.onEditSubmit}>
+        <input ref={name} type='text' defaultValue={name} onBlur={close} required/>
+      </form>
+    )
+
+  }
+}
+
+export default IngrediensHeaderEdit;
