c8fc3fb4925d98fcba645f89754d4591dbc8ba51
oanylund <ole.a.nylund@gmail.com>
2016-02-21 03:01:44 +0100

    Wrapped addingredientcategory blaze template with react component

---
+++ client/app/components/Ingredienser/AddIngredientCategory.jsx
@@ -0,0 +1,10 @@
+import React, { PropTypes } from 'react'
+import BlazeTemplate from '../BlazeTemplate.jsx'
+
+const addIngredientCategory = (props) => {
+  return (
+    <BlazeTemplate templateName='addIngredientCategory'/>
+  )
+}
+
+export default addIngredientCategory
