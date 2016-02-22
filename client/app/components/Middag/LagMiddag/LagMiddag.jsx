f9d922d23516da656c63301fb2927ce7f0d964a4
oanylund <ole.a.nylund@gmail.com>
2016-02-22 02:25:41 +0100

    Added component to add dinner. Just view template at the moment

--- client/app/components/Middag/LagMiddag/LagMiddag.jsx
+++ client/app/components/Middag/LagMiddag/LagMiddag.jsx
@@ -4,31 +4,58 @@ import { Grid, Row, Col, Button, ButtonToolbar, Alert } from 'react-bootstrap'
 class LagMiddag extends React.Component {
   render () {
     return (
-      <div className='marginSquare'>
+      <div className='marginSquare addDinner'>
         <Grid fluid>
           <Row>
             <Col md={12}>
-              <h2 style={{marginBottom:20,marginTop:0}}>Lag ny middag</h2>
+              <h2 className='addDinner-mainHeader'>Lag ny middag</h2>
             </Col>
           </Row>
           <Row>
             <Col sm={12}>
               <fieldset>
-                <legend>Legg til bilde, tittel og beskrivelse</legend>
-                <Col sm={3}>
-                  <div style={{textAlign:'center'}}>
-                    <img src="//placehold.it/200x200" className='img-thumbnail' />
+                <legend>Legg til tittel og beskrivelse</legend>
+                <div className='form-group'>
+                  <label className="control-label" htmlFor='newDinnerTitle'>Tittel</label>
+                  <input id='newDinnerTitle' className='form-control' type='text'/>
+                  <div className='help-block'>Testing</div>
+                </div>
+                <div className='form-group'>
+                  <label className="control-label" htmlFor='newDinnerDesc'>Beskrivelse</label>
+                  <textarea id='newDinnerDesc' className="form-control" ></textarea>
+                </div>
+              </fieldset>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12}>
+              <fieldset>
+                <legend>Legg til bilde</legend>
+                <div style={{textAlign:'center'}}>
+                  <img src="//placehold.it/200x200" className='img-thumbnail' />
+                </div>
+              </fieldset>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12}>
+              <fieldset className='addDinner-topMargin'>
+                <legend>Legg til ingredienser</legend>
+                <Col sm={12}>
+                  <div className='addDinner-IngredRow'>
+                    <div className='form-group addDinner-Antall'>
+                      <label className='control-label'>Antall</label>
+                      <input className='form-control' type='number'
+                        min={1} max={9999} maxLength={4} size={4} />
+                    </div>
+                    <span>Stykk</span>
+                    <div className='form-group addDinner-Antall'>
+                      <label className='control-label'>Ingrediens</label>
+                      <input className='form-control' type='text'/>
+                    </div>
+                    <i className='fa fa-close' />
                   </div>
-                </Col>
-                <Col sm={6}>
-                  <div className='form-group'>
-                    <label htmlFor='newDinnerTitle'>Tittel</label>
-                    <input id='newDinnerTitle' className='form-control' type="text"/>
-                  </div>
-                  <div className='form-group'>
-                    <label htmlFor='newDinnerDesc'>Beskrivelse</label>
-                    <textarea id='newDinnerDesc' className="form-control" ></textarea>
-                  </div>
+                  <Button bsStyle="primary" block><i className='fa fa-plus'/></Button>
                 </Col>
               </fieldset>
             </Col>
