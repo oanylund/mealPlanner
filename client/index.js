2ddda9f65e7c49f9229b4bf19407f5b2d9108a85
oanylund <ole.a.nylund@gmail.com>
2016-02-29 11:52:55 +0100

    Added reactive status alert. will notify user of connection status eventually

--- client/index.js
+++ client/index.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { render } from 'react-dom'
 import Routes from './routes.jsx'
 
-
-Meteor.subscribe('uker')
 Meteor.startup(function () {
   render(Routes(),document.getElementById('root'));
+});
+
+Tracker.autorun(function () {
+  if (Meteor.status().connected) {
+      console.log("Vi er connected");
+      // TODO: Add notification system to alert user of status
+  } else {
+      console.log("Vi er disconnected");
+  }
 });
