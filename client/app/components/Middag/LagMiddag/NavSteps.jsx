800def51740162100c458593b6605fdfaffb980c
oanylund <ole.a.nylund@gmail.com>
2016-03-02 13:45:32 +0100

    Created new component to abstract nav

---
+++ client/app/components/Middag/LagMiddag/NavSteps.jsx
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react'
+import { Nav, NavItem } from 'react-bootstrap'
+import ClassName from 'classnames'
+
+const NavSteps = (props) => {
+
+  let titleAndDescHasChanged = props.titleDescObj.titleHasBeenChanged && props.titleDescObj.descHasBeenChanged;
+
+  let titleAndDescClass = ClassName({
+    success: props.validSteps.titleAndDesc.valid && titleAndDescHasChanged,
+    error: !props.validSteps.titleAndDesc.valid && titleAndDescHasChanged,
+  })
+
+  return (
+    <Nav className='addDinner-nav'
+      bsStyle="tabs" activeKey={props.selectedView} onSelect={props.changeView}>
+      <NavItem eventKey={1} className={titleAndDescClass}>Tittel og beskrivelse</NavItem>
+      <NavItem eventKey={2} className=''>Ingredienser</NavItem>
+      <NavItem eventKey={3} className=''>Steg</NavItem>
+      <NavItem eventKey={4} className=''>Bilde</NavItem>
+    </Nav>
+  )
+}
+
+export default NavSteps
