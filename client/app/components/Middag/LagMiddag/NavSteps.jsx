da5e657ebaecfa87a8a24fae415c32005f397795
oanylund <ole.a.nylund@gmail.com>
2016-03-02 20:39:05 +0100

    Added validation state to ingredient tab

--- client/app/components/Middag/LagMiddag/NavSteps.jsx
+++ client/app/components/Middag/LagMiddag/NavSteps.jsx
@@ -11,11 +11,16 @@ const NavSteps = (props) => {
     error: !props.validSteps.titleAndDesc.valid && titleAndDescHasChanged,
   })
 
+  let ingredClass = ClassName({
+    success: props.validSteps.ingredients.valid && props.ingredObj.ingredHasBeenAdded,
+    error: !props.validSteps.ingredients.valid && props.ingredObj.ingredHasBeenAdded,
+  })
+
   return (
     <Nav className='addDinner-nav'
       bsStyle="tabs" activeKey={props.selectedView} onSelect={props.changeView}>
       <NavItem eventKey={1} className={titleAndDescClass}>Tittel og beskrivelse</NavItem>
-      <NavItem eventKey={2} className=''>Ingredienser</NavItem>
+      <NavItem eventKey={2} className={ingredClass}>Ingredienser</NavItem>
       <NavItem eventKey={3} className=''>Steg</NavItem>
       <NavItem eventKey={4} className=''>Bilde</NavItem>
     </Nav>
