6ebf4e7058af85919306f5da9730f8f4db6dda9e
oanylund <ole.a.nylund@gmail.com>
2016-03-02 02:18:58 +0100

    Added actions and handlers for adding title and description to dinner

--- client/app/actions/LagMiddagActions.js
+++ client/app/actions/LagMiddagActions.js
@@ -1,6 +1,8 @@
 import alt from "../alt"
 
 class LagMiddagActions {
-  
+  addTitleAndDesc(titleAndDesc) {
+    return titleAndDesc
+  }
 }
 export default alt.createActions(LagMiddagActions)
--- client/app/stores/LagMiddagStore.js
+++ client/app/stores/LagMiddagStore.js
@@ -1,9 +1,22 @@
 import alt from "../alt"
 import LagMiddagActions from '../actions/LagMiddagActions'
 
+const dinnerStartObj = {
+  title: '',
+  desc: ''
+}
+
+
 class LagMiddagStore {
   constructor() {
+
+    this.dinnerObj = dinnerStartObj
+
     this.bindActions(LagMiddagActions)
+  }
+  onAddTitleAndDesc(titleAndDesc) {
+    this.dinnerObj.title = titleAndDesc.title
+    this.dinnerObj.desc = titleAndDesc.desc
   }
 }
 
