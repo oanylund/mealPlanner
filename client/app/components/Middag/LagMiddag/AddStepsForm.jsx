3f0ef0537eadc1e88159058ce2c8ab59d8134cda
oanylund <ole.a.nylund@gmail.com>
2016-03-06 22:02:09 +0100

    Started shaping the AddStepsForm

--- client/app/components/Middag/LagMiddag/AddStepsForm.jsx
+++ client/app/components/Middag/LagMiddag/AddStepsForm.jsx
@@ -2,7 +2,15 @@ import React, { PropTypes } from 'react'
 
 const AddStepsForm = (props) => {
   return (
-    <div>Testform</div>
+    <div className='addDinner-StepAddForm'>
+      <div className='form-group'>
+        <label className='control-label'>Beskrivelse</label>
+        <textarea className='form-control'></textarea>
+      </div>
+      <div className='form-group' style={{textAlign: 'right'}}>
+        <input className='btn btn-primary addIngredBtn' type='button' value='Legg til'/>
+      </div>
+    </div>
   )
 }
 
