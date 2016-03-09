48d0dbdf07d79df44be3d3267c6fab354ba53147
oanylund <ole.a.nylund@gmail.com>
2016-03-10 00:20:50 +0100

    Changed AddStepForm to stateful component to get refs to focus texfield on mount

--- client/app/components/Middag/LagMiddag/AddStepsForm.jsx
+++ client/app/components/Middag/LagMiddag/AddStepsForm.jsx
@@ -1,17 +1,23 @@
 import React, { PropTypes } from 'react'
 
-const AddStepsForm = (props) => {
-  return (
-    <div className='addDinner-StepAddForm'>
-      <div className='form-group'>
-        <label className='control-label'>Beskrivelse</label>
-        <textarea className='form-control'></textarea>
+class AddStepsForm extends React.Component {
+  componentDidMount() {
+    this.refs.addStepTextArea.focus();
+  }
+  render () {
+    return (
+      <div className='addDinner-StepAddForm'>
+        <i className='fa fa-close' onClick={this.props.closeForm} />
+        <div className='form-group'>
+          <label className='control-label'>Beskriv nytt steg</label>
+          <textarea ref='addStepTextArea' className='form-control'></textarea>
+        </div>
+        <div className='form-group' style={{textAlign: 'right'}}>
+          <input className='btn btn-primary addIngredBtn' type='button' value='Legg til'/>
+        </div>
       </div>
-      <div className='form-group' style={{textAlign: 'right'}}>
-        <input className='btn btn-primary addIngredBtn' type='button' value='Legg til'/>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default AddStepsForm
+export default AddStepsForm;
