0c0e059f34cc6756391df9d39f2b8cc4f0cb5af0
oanylund <ole.a.nylund@gmail.com>
2016-03-09 23:47:50 +0100

    Added new Icons and methods to AddedStepItem

--- client/app/components/Middag/LagMiddag/AddedStepItem.jsx
+++ client/app/components/Middag/LagMiddag/AddedStepItem.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+import ClassNames from 'classnames'
 
 class AddedStepItem extends React.Component {
   constructor(props) {
@@ -6,27 +7,49 @@ class AddedStepItem extends React.Component {
     this.state = {
       editMode: false
     }
+    this.onEdit = this.onEdit.bind(this);
+    this.onExitEdit = this.onExitEdit.bind(this);
   }
-
+  onEdit() {
+    this.setState({
+      editMode: true
+    }, () => {
+      this.refs.stepEdit.focus();
+    });
+  }
+  onExitEdit() {
+    this.setState({
+      editMode: false
+    });
+  }
   render () {
 
-    const fakeProps = 'tetete blball e'
+    const fakeProps = 'tetete blball e';
 
-    const editView = <textarea className='addedStep-Desc editMode' defaultValue={fakeProps}></textarea>
+    const editView = <textarea ref='stepEdit' className='addedStep-Desc editMode' defaultValue={fakeProps} />;
 
-    const descView = this.state.editMode ? editView : <p className='addedStep-Desc'>{fakeProps}</p>
+    const descView = this.state.editMode ? editView : <p className='addedStep-Desc'>{fakeProps}</p>;
+
+    const stepClass = ClassNames('addDinner-addedStep', {
+      editTrue: this.state.editMode
+    })
 
     return (
-        <div className='addDinner-addedStep'>
+        <div className={stepClass}>
           <div className='addedStep-Order'>
-            <i className='fa fa-chevron-up' />
-            <span className='addedStep-Step'>10</span>
-            <i className='fa fa-chevron-down' />
+            <i title='Flytt steg ett hakk opp' className='fa fa-chevron-up' />
+            <span title='Nåværende steg' className='addedStep-Step'>10</span>
+            <i title='Flytt steg ett hakk ned' className='fa fa-chevron-down' />
           </div>
           {descView}
+          { this.state.editMode ?
           <div className='addedStep-Menu'>
-            <i className='fa fa-close' />
-            <i className='fa fa-edit' />
+            <i title='Lagre endring' className='fa fa-save' />
+            <i title='Forkast endring' className='fa fa-trash-o' onClick={this.onExitEdit} />
+          </div> : '' }
+          <div className='addedStep-Menu'>
+            <i title='Slett steg' className='fa fa-close' />
+            <i title='Endre Steg' className='fa fa-edit' onClick={this.onEdit} />
           </div>
         </div>
     )
