fea250e92c58d0b7c814be20f3b3c54aae0ebbb6
oanylund <ole.a.nylund@gmail.com>
2016-03-10 19:39:22 +0100

    Added validation to edit step, and action to store to change it on confirmation

--- client/app/components/Middag/LagMiddag/AddedStepItem.jsx
+++ client/app/components/Middag/LagMiddag/AddedStepItem.jsx
@@ -5,10 +5,13 @@ class AddedStepItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      editMode: false
+      editMode: false,
+      emptyErr: false
     }
     this.onEdit = this.onEdit.bind(this);
+    this.onSaveEdit = this.onSaveEdit.bind(this);
     this.onExitEdit = this.onExitEdit.bind(this);
+    this.onTxtChange = this.onTxtChange.bind(this);
   }
   onEdit() {
     this.setState({
@@ -17,35 +20,62 @@ class AddedStepItem extends React.Component {
       this.refs.stepEdit.focus();
     });
   }
+  onSaveEdit() {
+    if( !this.state.emptyErr ) {
+      this.props.editStep({
+        index: this.props.index,
+        newTxt: this.refs.stepEdit.value
+      });
+      this.setState({ editMode: false });
+    }
+  }
   onExitEdit() {
     this.setState({
       editMode: false
     });
+    this.refs.stepEdit.value = this.props.stepTxt;
+  }
+  onTxtChange() {
+    if ( this.refs.stepEdit.value === '' ) {
+      this.setState({ emptyErr: true });
+    }
+    else {
+      this.setState({ emptyErr: false });
+    }
   }
   render () {
-    const { addStep, deleteStep, editStep, moveStep, moveStepUp, moveStepDown, stepTxt, index } = this.props;
-    const editView = <textarea ref='stepEdit' className='addedStep-Desc editMode' defaultValue={stepTxt} />;
-    const descView = this.state.editMode ? editView : <p className='addedStep-Desc'>{stepTxt}</p>;
+    const { addStep, deleteStep, moveStep, moveStepUp, moveStepDown, stepTxt, index } = this.props;
+
     const stepClass = ClassNames('addDinner-addedStep', {
       editTrue: this.state.editMode
+    });
+    const saveEditBtnClass = ClassNames('fa', 'fa-save', {
+      emptyErr: this.state.emptyErr
+    });
+    const editTxtClass = ClassNames('addedStep-Desc', 'editMode', {
+      emptyErr: this.state.emptyErr
     })
 
+    const editView = <textarea ref='stepEdit' className={editTxtClass}
+                      onChange={this.onTxtChange}  defaultValue={stepTxt} />;
+    const descView = this.state.editMode ? editView : <p className='addedStep-Desc'>{stepTxt}</p>;
+    const saveChangeBtnTitle = this.state.emptyErr ? 'Kan lagre tomt steg' : 'Lagre endring'
     return (
         <div className={stepClass}>
           <div className='addedStep-Order'>
-            <i title='Flytt steg ett hakk opp' className='fa fa-chevron-up' />
+            <i title='Flytt steg ett hakk opp' className='fa fa-chevron-up' onClick={moveStepUp.bind(null,index)} />
             <span title='Nåværende steg' className='addedStep-Step'>{index+1}</span>
-            <i title='Flytt steg ett hakk ned' className='fa fa-chevron-down' />
+            <i title='Flytt steg ett hakk ned' className='fa fa-chevron-down' onClick={moveStepDown.bind(null,index)} />
           </div>
           {descView}
           { this.state.editMode ?
           <div className='addedStep-Menu'>
-            <i title='Lagre endring' className='fa fa-save' />
-            <i title='Forkast endring' className='fa fa-trash-o' onClick={this.onExitEdit} />
+            <i title={saveChangeBtnTitle} className={saveEditBtnClass} onClick={this.onSaveEdit} />
+            <i title='Forkast endring' className='fa fa-close' onClick={this.onExitEdit} />
           </div>
           : '' }
           <div className='addedStep-Menu'>
-            <i title='Slett steg' className='fa fa-close' onClick={deleteStep.bind(null,index)} />
+            <i title='Slett steg' className='fa fa-trash-o' onClick={deleteStep.bind(null,index)} />
             <i title='Endre Steg' className='fa fa-edit' onClick={this.onEdit} />
           </div>
         </div>
