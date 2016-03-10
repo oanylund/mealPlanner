d1eb6deaab2b5dc29d33a755070756d82fb4e6ea
oanylund <ole.a.nylund@gmail.com>
2016-03-10 01:04:54 +0100

    Added functionallity to addstepsform

--- client/app/components/Middag/LagMiddag/AddStepsForm.jsx
+++ client/app/components/Middag/LagMiddag/AddStepsForm.jsx
@@ -1,19 +1,62 @@
 import React, { PropTypes } from 'react'
+import ClassNames from 'classnames'
 
 class AddStepsForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      hasBeenChanged: false,
+      emptyErr: false
+    }
+    this.textChange = this.textChange.bind(this);
+    this.validateAndInsert = this.validateAndInsert.bind(this);
+  }
   componentDidMount() {
     this.refs.addStepTextArea.focus();
   }
+  textChange() {
+    const formText = this.refs.addStepTextArea.value;
+    const emptyTxt = formText === '';
+    this.setState({
+      text: formText,
+      hasBeenChanged: true,
+      emptyErr: emptyTxt
+    });
+  }
+  validateAndInsert() {
+    const { text, hasBeenChanged, emptyErr } = this.state;
+    if( !hasBeenChanged && text === '' ) {
+      this.setState({
+        hasBeenChanged: true,
+        emptyErr: true
+      })
+    }
+    else {
+      // TODO: Call Action insert
+    }
+  }
   render () {
+    const { text, hasBeenChanged, emptyErr } = this.state;
+
+    const emptyMsg = 'Stegbeskrivelse kan ikke være tom.';
+    const textFieldClass = ClassNames('form-group',{
+      'has-error': emptyErr,
+      'has-success': hasBeenChanged && !emptyErr
+    })
+    const btnStyle = ClassNames()
     return (
       <div className='addDinner-StepAddForm'>
         <i className='fa fa-close' onClick={this.props.closeForm} />
-        <div className='form-group'>
+        <div className={textFieldClass}>
           <label className='control-label'>Beskriv nytt steg</label>
-          <textarea ref='addStepTextArea' className='form-control'></textarea>
+          <textarea ref='addStepTextArea' className='form-control'
+            onChange={this.textChange} value={text} />
+          { emptyErr ? <div className='help-block'>{emptyMsg}</div> : '' }
         </div>
         <div className='form-group' style={{textAlign: 'right'}}>
-          <input className='btn btn-primary addIngredBtn' type='button' value='Legg til'/>
+          <input className='btn btn-primary addIngredBtn' type='button'
+            value='Legg til' disabled={emptyErr} onClick={this.validateAndInsert}/>
         </div>
       </div>
     )
