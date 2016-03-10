b47bbf14151caa70a00bb700639e9b898a9339d2
oanylund <ole.a.nylund@gmail.com>
2016-03-10 02:02:23 +0100

    Added props to ShowStepsAdded. Added focus to plusBtn when form is closed

--- client/app/components/Middag/LagMiddag/AddSteps.jsx
+++ client/app/components/Middag/LagMiddag/AddSteps.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 import AddStepsForm from './AddStepsForm.jsx'
-import AddedStepItem from './AddedStepItem.jsx'
+import ShowStepsAdded from './ShowStepsAdded.jsx'
 import InfoAlert from '../../Reusable/InfoAlert.jsx'
 import PlusBtn from '../../Reusable/PlusBtn.jsx'
 
@@ -22,12 +22,22 @@ class AddSteps extends React.Component {
   hideAddForm() {
     this.setState({
       showAddForm: false
+    }, () => {
+      this.refs.showAddStepFormBtn.focusBtn();
     })
   }
   render () {
-    const stepsInStore =  1; //this.props.dinnerObj.ingredients.length;
+    const passToSteps = {
+      steps: this.props.dinnerObj.steps,
+      deleteStep: this.props.deleteStep,
+      editStep: this.props.editStep,
+      moveStep: this.props.moveStep,
+      moveStepUp: this.props.moveStepUp,
+      moveStepDown: this.props.moveStepDown,
+    }
+    const stepsInStore =  this.props.dinnerObj.steps.length;
     const alertEmpty = <InfoAlert txt='Trykk + knappen for å legge til steg' /> ;
-    const showSteps = stepsInStore === 0 ? alertEmpty : <AddedStepItem /> ;
+    const showSteps = stepsInStore === 0 ? alertEmpty : <ShowStepsAdded {...passToSteps} /> ;
 
     return (
       <fieldset className={this.props.showSteps}>
