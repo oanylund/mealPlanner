3a91778efd32f1a36773f455e9891759ff3aad61
oanylund <ole.a.nylund@gmail.com>
2016-03-12 22:37:38 +0100

    Added validation color to image tab in navsteps component when thumb is added

--- client/app/components/Middag/LagMiddag/NavSteps.jsx
+++ client/app/components/Middag/LagMiddag/NavSteps.jsx
@@ -20,13 +20,17 @@ const NavSteps = (props) => {
     success: props.dinnerObj.steps.length > 0
   })
 
+  let imgClass = ClassName({
+    success: props.validSteps.image.added
+  })
+
   return (
     <Nav className='addDinner-nav'
       bsStyle="tabs" activeKey={props.selectedView} onSelect={props.changeView}>
       <NavItem eventKey={1} className={titleAndDescClass}>Tittel og beskrivelse</NavItem>
       <NavItem eventKey={2} className={ingredClass}>Ingredienser</NavItem>
       <NavItem eventKey={3} className={stepClass}>Steg</NavItem>
-      <NavItem eventKey={4} className=''>Bilde</NavItem>
+      <NavItem eventKey={4} className={imgClass}>Bilde</NavItem>
     </Nav>
   )
 }
