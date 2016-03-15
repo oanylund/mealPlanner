575fe455ac33b5bc8ac3172719d760f4bfc30ef4
oanylund <ole.a.nylund@gmail.com>
2016-03-16 00:25:42 +0100

    Integrated compomoser that passes a dinner array to the ListDinners component

--- client/app/components/Middag/ListDinners.jsx
+++ client/app/components/Middag/ListDinners.jsx
@@ -1,14 +1,26 @@
 import React, { PropTypes } from 'react'
 import { Row, Col } from 'react-bootstrap'
+import { composeWithTracker } from 'react-komposer'
 import DinnerItem from './DinnerItem.jsx'
+import DinnersListData from '../../composers/DinnersListData'
+import Spinner from '../spinner.jsx'
 
-const ListDinners = (props) => {
+const ListDinners = ({dinners}) => {
+
+  const dinnerList = dinners.map( (dinner) => {
+    return (
+      <Col md={6} >
+        <DinnerItem title={dinner.title} description={dinner.description}
+          linkUrl={`/middager/${dinner._id}`} />
+      </Col>
+    )
+  })
+
   return (
     <Row>
-      <DinnerItem title='Taco' description='Testing flere ganger opptil 12 tretten'
-        linkUrl='test' />
+      {dinnerList}
     </Row>
   )
 }
 
-export default ListDinners
+export default composeWithTracker(DinnersListData, Spinner)(ListDinners)
