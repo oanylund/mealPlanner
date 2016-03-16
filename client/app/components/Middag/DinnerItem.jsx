ef76c77fdc3f55bc50cc3436d42dc259610e3338
oanylund <ole.a.nylund@gmail.com>
2016-03-16 01:56:01 +0100

    Made thumbnail its own component and switched to react-router Link on see more..

--- client/app/components/Middag/DinnerItem.jsx
+++ client/app/components/Middag/DinnerItem.jsx
@@ -1,16 +1,19 @@
 import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
+import DinnerItemImage from './DinnerItemImage.jsx'
 
 const DinnerItem = (props) => {
-  let img = props.imgUrl ? props.imgUrl : '/images/default-dinner.png'
+  let img = props.imageId ? <DinnerItemImage imageId={props.imageId} />
+    : <img className='dashWidgetImg' src='/images/default-dinner.png'/>;
   return (
     <div className='dashWidgetBox'>
-      <img className='dashWidgetImg' src={img} alt="" />
+      {img}
       <div className='dashWidgetBody'>
         <h3>{props.title}</h3>
         <span>{props.description}</span><br/>
         {props.descriptionGrey ? <span className='grey'>{props.descriptionGrey}</span> : '' }
         { props.linkUrl ?
-        <a href={props.linkUrl}>Se mer...</a>
+        <Link to={props.linkUrl}>Se Mer...</Link>
         : '' }
       </div>
     </div>
