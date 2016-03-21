e722eb3c2b46b61a0eb3197c43a8befb55c6daad
oanylund <ole.a.nylund@gmail.com>
2016-03-21 16:50:25 +0100

    Made main App component a extended class, instead of stateless because HTMLBackend for dnd uses refs

--- client/app/components/App.jsx
+++ client/app/components/App.jsx
@@ -3,15 +3,17 @@ import Sidebar from './Sidebar/Sidebar.jsx'
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
-const mealPlanner = ({children}) => {
+class mealPlanner extends React.Component {
+  render () {
     return (
       <div id='mealWrapper'>
         <Sidebar/>
         <div id='mainContent'>
-          {children}
+          {this.props.children}
         </div>
       </div>
     )
+  }
 }
 
 export default DragDropContext(HTML5Backend)(mealPlanner)
