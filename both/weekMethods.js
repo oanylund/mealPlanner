3a74d9062088821d57427abfcf80811e9f0c0249
oanylund <ole.a.nylund@gmail.com>
2016-04-01 23:55:48 +0200

    Added metods to fetch dinners to chose when making a new week

--- both/weekMethods.js
+++ both/weekMethods.js
@@ -5,5 +5,20 @@ Meteor.methods({
       return true
     }
     return false
+  },
+  getDinnerCount: function(titleFilter) {
+    if( titleFilter === '') {
+      return Middager.find({}).count();
+    }
+    else {
+      return Middager.searchTitle(titleFilter).count();
+    }
+  },
+  getDinners: function( offset, limit, titleFilter ) {
+    if( titleFilter === '' ) {
+      return Middager.find({}, { limit: limit, skip: offset }).fetch();
+    } else {
+      return Middager.searchTitle(titleFilter).fetch();
+    }
   }
 });
