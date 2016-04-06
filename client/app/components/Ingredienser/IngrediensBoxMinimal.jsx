201f4a1e0eba569d8092840f1ef72ce2ddd1ece6
oanylund <ole.a.nylund@gmail.com>
2016-04-07 00:05:55 +0200

    Changed ingredient view. When ingredient is in use in a dinner, you now cant delete the ingredient

--- client/app/components/Ingredienser/IngrediensBoxMinimal.jsx
+++ client/app/components/Ingredienser/IngrediensBoxMinimal.jsx
@@ -1,6 +1,12 @@
 import React, { PropTypes } from 'react'
 
 const IngrediensBoxMinimal = ({ingredient,click,btns}) => {
+  const canBeDeleted = !ingredient.usedInDinners || ingredient.usedInDinners.length === 0;
+  const cantDeleteStyle = {
+    color: '#CCC'
+  }
+  const cantDeleteTitle = 'Ingrediensen brukes i en eller flere middager og kan ikke slettes';
+
   return (
     <div className='ingrediensBox ingrediensBox-zoomIn' onClick={click}>
       <div className='ingrediensBox-Col'>
@@ -15,7 +21,9 @@ const IngrediensBoxMinimal = ({ingredient,click,btns}) => {
       </div>
       <div className='ingrediensBox-MenuCol'>
         <i className='fa fa-edit ingrediensBox-MenuItem' onClick={btns.edit.bind(null,ingredient._id)}/>
-        <i className='fa fa-close ingrediensBox-MenuItem' onClick={btns.remove.bind(null,ingredient._id)} />
+        { canBeDeleted ?
+          <i className='fa fa-close ingrediensBox-MenuItem' onClick={btns.remove.bind(null,ingredient._id)} />
+        : <i title={cantDeleteTitle} className='fa fa-close ingrediensBox-MenuItem' style={cantDeleteStyle} /> }
       </div>
     </div>
   )
