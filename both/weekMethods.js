deab4335241fed761625e49a8d2a961c20524d9c
oanylund <ole.a.nylund@gmail.com>
2016-04-18 23:52:04 +0200

    Added week shoppinglist dependency methods

--- both/weekMethods.js
+++ both/weekMethods.js
@@ -2,4 +2,14 @@ Meteor.methods({
   addWeek: (newWeek) => {
     return Uker.insert(newWeek);
   },
+  addWeekShopListDep: (weekId,shopListId) => {
+    Uker.update(weekId, {
+      $addToSet: { usedInShopList: shopListId }
+    });
+  },
+  removeWeekShopListDep: (weekId,shopListId) => {
+    Uker.update(weekId, {
+      $pull: { usedInShopList: shopListId }
+    });
+  }
 });
