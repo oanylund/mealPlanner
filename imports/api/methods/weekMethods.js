7d7f79693d4e109196dd586ba9952578e935bb46
oanylund <ole.a.nylund@gmail.com>
2016-08-08 20:43:08 +0200

    added methods for deleting days in dinner and week itself

--- imports/api/methods/weekMethods.js
+++ imports/api/methods/weekMethods.js
@@ -1,9 +1,26 @@
 import { Meteor } from 'meteor/meteor';
 import Uker from '../collections/uker';
+import Middager from '../collections/middag';
 
 Meteor.methods({
   addWeek: (newWeek) => {
     return Uker.insert(newWeek);
+  },
+  deleteWeek: (weekId) => {
+    Uker.remove(weekId, () => {
+      Middager.update({}, {
+        $pull: { usedInWeek: weekId }
+      },
+      {
+        multi: true
+      });
+
+    });
+  },
+  deleteDay: (day, weekId) => {
+    Uker.update(weekId, {
+      $unset: { [`days.${day}`]: '' }
+    });
   },
   addWeekShopListDep: (weekId,shopListId) => {
     Uker.update(weekId, {
