cecef488962d04dac4831bca772ae4d7569f30bc
oanylund <ole.a.nylund@gmail.com>
2016-08-24 21:25:02 +0200

    Updated displayweekwithdata with new react-apollo api from updated module

--- imports/ui/app/containers/DisplayWeekWithData.js
+++ imports/ui/app/containers/DisplayWeekWithData.js
@@ -1,35 +1,36 @@
-import { connect } from 'react-apollo';
+import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import DisplayWeek from '../components/Uker/VisUke/DisplayWeek.jsx';
 
-const DisplayWeekWithData = connect({
-  mapQueriesToProps: ({ ownProps }) => ({
-    data: {
-      query: gql`
-        query week($id: String!) {
-          week(id: $id) {
-            name
-            days {
-              day
-              whynot
-              comment
-              dinner {
-                _id
-                title
-                description
-                image {
-                  url
-                }
-              }
-            }
+const WEEK_QUERY = gql`
+  query week($id: String!) {
+    week(id: $id) {
+      name
+      days {
+        day
+        whynot
+        comment
+        dinner {
+          _id
+          title
+          description
+          image {
+            url
           }
         }
-      `,
-      variables: {
-        id: ownProps.routeParams.ukeId
-      },
+      }
     }
-  })
+  }
+`;
+
+const DisplayWeekWithData = graphql(WEEK_QUERY, {
+    options(ownProps) {
+      return {
+        variables: {
+          id: ownProps.routeParams.ukeId
+        }
+      }
+    }
 })(DisplayWeek)
 
 export default DisplayWeekWithData;
