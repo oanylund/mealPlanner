5ec2df3d7a41c70f74f5600aa471da5b1986f3ea
oanylund <ole.a.nylund@gmail.com>
2016-08-26 21:52:29 +0200

    Added addIngredient method to ingredMethods

--- imports/api/methods/ingredMethods.js
+++ imports/api/methods/ingredMethods.js
@@ -3,6 +3,19 @@ import Ingredienser from '../collections/ingredienser';
 import IngrediensKat from '../collections/IngrediensKategori';
 
 Meteor.methods({
+  addIngredient: (newIngred) => {
+    Ingredienser.insert({
+      name: {
+        singular: newIngred.ingredSingular,
+        plural: newIngred.ingredPlural
+      },
+      unit: {
+        singular: newIngred.unitSingular,
+        plural: newIngred.unitPlural
+      },
+      categoryId: newIngred.category
+    })
+  },
   removeIngredient: (id) => {
     Ingredienser.remove(id)
   },
